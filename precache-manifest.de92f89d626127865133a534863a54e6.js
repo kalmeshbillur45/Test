cd53bee9ed93fe1ff0a9471f83a5d472a645dc42
Kalmesh Billur <M1057969@mindtree.com>
2020-10-06 16:42:36 +0530

    Updates

---
+++ precache-manifest.de92f89d626127865133a534863a54e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87cf847b93750d7209213d92e89c7f02",
+    "url": "/Test/index.html"
+  },
+  {
+    "revision": "2308cae15d337760e4de",
+    "url": "/Test/static/css/main.c83d9cf0.chunk.css"
+  },
+  {
+    "revision": "cc18e0bb7e4f984dd7ea",
+    "url": "/Test/static/js/2.29f19ccb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Test/static/js/2.29f19ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2308cae15d337760e4de",
+    "url": "/Test/static/js/main.88d767db.chunk.js"
+  },
+  {
+    "revision": "8c0654e15ff8a8554cc8",
+    "url": "/Test/static/js/runtime-main.6e33c712.js"
+  }
+]);
